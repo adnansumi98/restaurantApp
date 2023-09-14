17fa3bc4fdcc3803be6d7edf3ee63029d8d46cb3
Bhavani Shankar <mbs.nitrr@gmail.com>
2023-09-14 10:47:19 +0000

    carttests

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ function App() {
           <Switch>
             <Route exact path="/login" component={Login} />
             <ProtectedRoute exact path="/" component={Home} />
-            <ProtectedRoute exact path="/Cart" component={Cart} />
+            <ProtectedRoute exact path="/cart" component={Cart} />
             <Route path="/not-found" component={NotFound} />
             <Redirect to="/not-found" />
           </Switch>
